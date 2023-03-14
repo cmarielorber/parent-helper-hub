9e75fb7a0be58f6c65d67dd9ba5f96f866ecb842
heidiwu3388 <heidiwu3388@yahoo.com>
2023-03-14 02:07:45 -0700

    trying the SchoolDigger API to get all schools

--- client/src/pages/Schools.js
+++ client/src/pages/Schools.js
@@ -1,5 +1,24 @@
+import React, {useEffect} from 'react';
 
 function Schools() {
+
+    useEffect(() => {
+        let apiURL = `https://api.schooldigger.com/v2.0/schools`;
+            apiURL+=`?st=CA`;
+            apiURL+=`&city=San%20Diego`;
+            apiURL+=`&zip=92130`;
+            apiURL+=`&level=Elementary`;
+            apiURL+=`&appID=${process.env.REACT_APP_API_ID}`;
+            apiURL+=`&appKey=${process.env.REACT_APP_API_KEY}`;
+        console.log(apiURL);
+        
+        fetch(apiURL, { mode: 'no-cors'})
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+
+    }, []);
+
     return (
         <div>
             <h1>School page</h1>
