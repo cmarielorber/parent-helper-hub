72b0e76f32389fcc776186acc75422dad33f2ee4
hannybear88 <hannahkchung88@gmail.com>
2023-03-15 12:50:28 -0700

    modified code in client/src/components/EditForm.js file and client/src/components/Proile.js file

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -9,9 +9,10 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 {/* <button type="button" class="btn btn-success mx-3" data-mdb-ripple-color="dark" 
             onClick={edit form?} 
-            >Edit</button>  */}
+            >Edit</button>   */}
 
 const EditForm = () => {
+  console.log("inside EditForm");
   // set initial form state
   const [userFormData, setUserFormData] = useState({
     username: "",
@@ -104,138 +105,138 @@ const EditForm = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
+  return (<h1>Hello</h1>)
+  // return (
+  //   <>
+  //     {/* This is needed for the validation functionality above */}
+  //     <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+  //       {/* show alert if server response is bad */}
+  //       <Alert
+  //         dismissible
+  //         onClose={() => setShowAlert(false)}
+  //         show={showAlert}
+  //         variant="danger"
+  //       >
+  //         Something went wrong with your signup!
+  //       </Alert>
 
-  return (
-    <>
-      {/* This is needed for the validation functionality above */}
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
-        <Alert
-          dismissible
-          onClose={() => setShowAlert(false)}
-          show={showAlert}
-          variant="danger"
-        >
-          Something went wrong with your signup!
-        </Alert>
+  //       <Form.Group>
+  //         <Form.Label htmlFor="username">Username</Form.Label>
+  //         <Form.Control
+  //           type="text"
+  //           placeholder="Your username"
+  //           name="username"
+  //           onChange={handleInputChange}
+  //           value={userFormData.username}
+  //           required
+  //         />
+  //         <Form.Control.Feedback type="invalid">
+  //           Username is required!
+  //         </Form.Control.Feedback>
+  //       </Form.Group>
+  //       <Form.Group>
+  //         <Form.Label htmlFor="ageGroup">
+  //           Child's Age Group
+  //         </Form.Label>
+  //         <Form.Control i
+  //           id="ageGroup" 
+  //           as="select"
+  //           type='string'
+  //           placeholder='Child Age Group'
+  //           name='ageGroup'
+  //           onChange={handleInputChange}
+  //           value={userFormData.ageGroup}
+  //           required
+  //         >
+  //          <option value="0-5">0-5</option>
+  //          <option value="6-18">6-18</option>
+  //          <option value="18+">18+</option>
+  //         </Form.Control>
+  //       </Form.Group>
+  //       <Form.Group>
+  //         <Form.Label htmlFor="kidCount">
+  //           Number of Kids
+  //         </Form.Label>
+  //         <Form.Control i
+  //           id="kidCount" 
+  //           as="select"
+  //           type='number'
+  //           placeholder='Number of kids'
+  //           name='kidCount'
+  //           onChange={handleInputChange}
+  //           value={userFormData.kidCount}
+  //           required
+  //         >
+  //          <option value="1">1</option>
+  //          <option value="2">2</option>
+  //          <option value="3">3</option>
+  //          <option value="4">4</option>
+  //          <option value="5">5</option>
+  //          <option value="6">6</option>
+  //          <option value="7">7</option>
+  //         </Form.Control>
+  //       </Form.Group>
+  //       <Form.Group>
+  //         <Form.Label htmlFor="kids">Child's Full Name</Form.Label>
+  //         {
+  //           renderNameForm()
+  //         }
+  //       </Form.Group>
+  //       <Form.Label htmlFor="zipcode">Zipcode</Form.Label>
+  //       <Form.Control
+  //         type="string"
+  //         placeholder="Your zipcode"
+  //         name="zipcode"
+  //         onChange={handleInputChange}
+  //         value={userFormData.zipcode}
+  //         required
+  //       />
 
-        <Form.Group>
-          <Form.Label htmlFor="username">Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your username"
-            name="username"
-            onChange={handleInputChange}
-            value={userFormData.username}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Username is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="ageGroup">
-            Child's Age Group
-          </Form.Label>
-          <Form.Control i
-            id="ageGroup" 
-            as="select"
-            type='string'
-            placeholder='Child Age Group'
-            name='ageGroup'
-            onChange={handleInputChange}
-            value={userFormData.ageGroup}
-            required
-          >
-           <option value="0-5">0-5</option>
-           <option value="6-18">6-18</option>
-           <option value="18+">18+</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="kidCount">
-            Number of Kids
-          </Form.Label>
-          <Form.Control i
-            id="kidCount" 
-            as="select"
-            type='number'
-            placeholder='Number of kids'
-            name='kidCount'
-            onChange={handleInputChange}
-            value={userFormData.kidCount}
-            required
-          >
-           <option value="1">1</option>
-           <option value="2">2</option>
-           <option value="3">3</option>
-           <option value="4">4</option>
-           <option value="5">5</option>
-           <option value="6">6</option>
-           <option value="7">7</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="kids">Child's Full Name</Form.Label>
-          {
-            renderNameForm()
-          }
-        </Form.Group>
-        <Form.Label htmlFor="zipcode">Zipcode</Form.Label>
-        <Form.Control
-          type="string"
-          placeholder="Your zipcode"
-          name="zipcode"
-          onChange={handleInputChange}
-          value={userFormData.zipcode}
-          required
-        />
+  //       <Form.Group>
+  //         <Form.Label htmlFor="email">Email</Form.Label>
+  //         <Form.Control
+  //           type="email"
+  //           placeholder="Your email address"
+  //           name="email"
+  //           onChange={handleInputChange}
+  //           value={userFormData.email}
+  //           required
+  //         />
+  //         <Form.Control.Feedback type="invalid">
+  //           Email is required!
+  //         </Form.Control.Feedback>
+  //       </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor="email">Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Your email address"
-            name="email"
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Email is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Your password"
-            name="password"
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Password is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={
-            !(
-              userFormData.username &&
-              userFormData.email &&
-              userFormData.password
-            )
-          }
-          type="submit"
-          variant="success"
-        >
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
+  //       <Form.Group>
+  //         <Form.Label htmlFor="password">Password</Form.Label>
+  //         <Form.Control
+  //           type="password"
+  //           placeholder="Your password"
+  //           name="password"
+  //           onChange={handleInputChange}
+  //           value={userFormData.password}
+  //           required
+  //         />
+  //         <Form.Control.Feedback type="invalid">
+  //           Password is required!
+  //         </Form.Control.Feedback>
+  //       </Form.Group>
+  //       <Button
+  //         disabled={
+  //           !(
+  //             userFormData.username &&
+  //             userFormData.email &&
+  //             userFormData.password
+  //           )
+  //         }
+  //         type="submit"
+  //         variant="success"
+  //       >
+  //         Submit
+  //       </Button>
+  //     </Form>
+  //   </>
+  // );
 };
 
 export default EditForm;
