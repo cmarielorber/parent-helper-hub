5a8e1651ced56d512cfe4fa31fbafb2f49277349
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2023-03-19 19:37:39 -0700

    footer is DONE

--- client/src/components/Donation.js
+++ client/src/components/Donation.js
@@ -1,34 +1,31 @@
-
 import React, { useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 function DonationForm() {
-  const [donationAmount, setDonationAmount] = useState(0);
+  const [donationAmount] = useState(0);
 
   function handleToken(token) {
     console.log(token);
     // Send the token to your backend to process the payment
   }
 
-  function handleCustomDonation(event) {
-    setDonationAmount(event.target.value);
-  }
 
   return (
-    <div>
+    <div className="donate">
       <StripeCheckout
+        style={{ display: "inline-block", maxWidth: "100px" }}
         stripeKey="pk_test_51Mm6IsCH8zpe9aZt5yJ8eyXk6PowgPwXpROkJy7kDkKH4xv2l8YWc88LoFv5JBq3lhFk3OFjv9tV5pgXpOYVlpEk00SdkFWV4q"
         token={handleToken}
         amount={donationAmount * 100}
-        name="Donations"
+        className="donations"
         description="For our Parent Community"
         currency="USD"
       >
-        <input type="number" min="1" onChange={handleCustomDonation} value={donationAmount} />
-        <button>Donate Custom Amount</button>
+        <button className="donations">Donate Custom Amount</button>
       </StripeCheckout>
     </div>
   );
+  
 }
 
 export default DonationForm;
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,39 +1,71 @@
-import React, { useState } from "react";
-import {
-  Box,
-  FooterContainer,
-  Row,
-  Column,
-  FooterLink,
-  Heading,
-} from "./Footerstyles";
+import { Container, Row, Col } from "react-bootstrap";
 import "../styles/pages.css";
+import DonationForm from "./Donation";
 import { GithubIcon } from "./icons";
 
 const Footer = () => {
-
   return (
-    <Box>
-      <FooterContainer>
+    <div className="footer">
+      <Container>
         <Row>
-          <Column>
-            <Heading>About Us</Heading>
-            <FooterLink href="#">Vision</FooterLink>
-            <FooterLink href="#">Contact Us</FooterLink>
-          </Column>
-
-          <Column>
-            <FooterLink href="https://github.com/cmarielorber/parent-helper-hub/" style={{ display: "inline-block" }}>
-            <GithubIcon />
-            </FooterLink>
-          </Column>
-          <Column>
-            <Heading>Donations are Welcome!</Heading>
-            <FooterLink href="#">Click Here To Donate!</FooterLink>
-          </Column>
+          <Col md={4}>
+            <h5>Our Team</h5>
+            <div className="team">
+              <div className="team-member">
+                <a className="githublink" href="https://github.com/hannybear88">
+                  <p className="first-name">Hannah</p>
+                  <p className="last-name">Chung</p>
+                </a>
+              </div>
+              <div className="team-member">
+                <a className="githublink" href="https://github.com/TrianaD">
+                  <p className="first-name">Triana</p>
+                  <p className="last-name">Deguzman</p>
+                </a>
+              </div>
+              <div className="team-member">
+                <a className="githublink" href="https://github.com/cmarielorber">
+                  <p className="first-name">Christen</p>
+                  <p className="last-name">Lorber</p>
+                </a>
+              </div>
+              <div className="team-member">
+                <a className="githublink" href="https://github.com/fmaldmed">
+                  <p className="first-name">Fernando</p>
+                  <p className="last-name">Maldonado</p>
+                </a>
+              </div>
+              <div className="team-member">
+                <a className="githublink" href="https://github.com/heidiwu3388">
+                  <p className="first-name">Heidi</p>
+                  <p className="last-name">Wu</p>
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col md={4}>
+            <h5>Connect</h5>
+            <a href="https://github.com/cmarielorber/">
+              <GithubIcon className="github-icon" />
+            </a>
+          </Col>
+          <Col md={4}>
+            <h5>Donations Accepted</h5>
+            <DonationForm />
+          </Col>
         </Row>
-      </FooterContainer>
-    </Box>
+      </Container>
+      <div className="footer-bottom">
+        <Container>
+          <Row>
+            <Col>
+              <p>© 2023 Parent Helper Hub</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 };
+
 export default Footer;
